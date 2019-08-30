8275f50f780c5c9a07a420663ac39ff612257720
ratexx <ratexx@gmail.com>
2019-08-30 15:23:06 +0700

    update call def

--- gui_hello.py
+++ gui_hello.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-
+import functools
 
 def on_click_btn():
     messagebox.showinfo("title is here","Aloha")
@@ -12,6 +12,7 @@ frame = ttk.Frame(root,width=300,height=200)
 frame.pack()
 
 btn = ttk.Button(frame, text="Hello",command=on_click_btn)
+#command=functools.partial(on_click_btn,param="a"  <<< call function
 btn.place(x=100,y=80 )
 
 
