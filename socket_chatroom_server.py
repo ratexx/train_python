6437470e9d320a2c7eaec4c009b13e0184abcd25
ratexx <ratexx@gmail.com>
2019-08-30 16:01:28 +0700

    socket connection

---
+++ socket_chatroom_server.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from socket import *
+from threading import Thread
+from chatroom import ChatRoom
+
+
+class MyThread(Thread):
+    def __init__(self, chat_room_socket):
+        super().__init__()
+
+        self.chat_room_socket = chat_room_socket
+
+    def run(self):
+        try:
+            while True:
+                self.chat_room_socket.append_text(
+                    bytes.decode(self.chat_room_socket.socket.recv(1024)))
+        except (ConnectionAbortedError, ConnectionResetError):
+            pass
+
+
+class ChatRoomSocket(ChatRoom):
+    def __init__(self, parent, psocket, pname):
+        super().__init__(parent, pname)
+
+        self.socket = psocket
+
+        MyThread(self).start()
+
+    def on_enter_text(self, event):
+        sinput = self.str_input
+
+        self.socket.send(str.encode(self.name + ": " + sinput.get()))
+
+        super().on_enter_text(event)
+
+
+# Main
+server = socket()
+ip = "127.0.0.1"
+port = 8888
+
+try:
+    server.bind((ip, port))
+    server.listen(10)
+    socket, address = server.accept()
+
+    name = "Server"
+except OSError:
+    socket = server
+    socket.connect((ip, port))
+
+    name = "Client"
+
+root = Tk()
+root.title("ChatRoom - " + name)
+
+ChatRoomSocket(root, socket, name)
+
+root.mainloop()
+
+socket.close()
